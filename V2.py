fd16a9d5a05d81cfd98c3449f92889c94ecf0c87
Exelion <dw14089@my.bristol.ac.uk>
2014-12-28 19:49:58 +0000

    First working prototype.

---
+++ V2.py
@@ -0,0 +1,87 @@
+#Basic 2 Player  Noughts and Crosses Game
+#...................,.....,-~*~,,
+#......................./:.:.:.:.:.|
+#......................|;.;.;.;.;./
+#......................|.;.;.;.;.|
+#............._,,,,,_.).;.;.;.;.|
+#.........,,-":.:.:.:."~-,;.;.;.|
+#........(_,,,,---,,_:.:.);.;.;..",,
+#......,-":.:.:.:.:.""-,,/;.;.;.;.;.",
+#.....(:.__,,,,,,,,,___);.;.;.;.;.;|
+#...../"":.:.:.:.:.:.:""\;.;.;.;.;.,"
+#....\",__,,,,,,,,,,,__/;;;;;;;;;/\
+#.....\.::.:.:.:.:.:.:.;.);;;;;;;;;/:\
+#.......\,,,,,---~~~~;;;;;;;;,"::::\
+#.........."""~~--,,,,,,,,,,-"::::::::::\
+import sys
+
+Players = [("Player1",1)] #first player in this list will be the player whose turn it is. if a players turn is over then they are removed and appended to the end.
+#players is a list of tuples, first element name second element symbol to represent player choices.
+
+Grid = [[0,0,0],[0,0,0],[0,0,0]]
+
+def OpponentChoiceState():
+	Choice = input("[H]uman or [C]omputer opponent ?")
+	if Choice == "H":					#change state
+		Players.append(("Player2",2))
+	elif Choice == "C":
+		Players.append(("Computer",3))
+	else: 
+		print ("Level 8 OSI Error")
+		return #exits function and goes back to where it was, loop repeats in this case.
+	print ("change current state")
+	return PlayerTurnState #state switch
+	
+
+def PrintGrid():
+	for Row in range(len(Grid)):
+		for Col in range(len(Grid)):
+			print (Grid[Row][Col], end=" ")
+		print(" ")
+	
+	
+def PromptPlayerMove():
+	print ("Make your move player.")
+	X = int(input("X"))-1
+	Y = int(input("Y"))-1
+	return X,Y
+	
+def HasPlayerWon(Symbol):
+	  
+	for Row in range(3): #execute this block of code for each row.Holds true output in PlayerWon
+		PlayerWon = Grid[Row][0] == Symbol and Grid[Row][1] == Symbol and Grid[Row][2] == Symbol # checks each horizontal, AND logic gate, returns true or false
+		PlayerWon = PlayerWon or Grid[0][Row] == Symbol and Grid[1][Row] == Symbol and Grid[2][Row] == Symbol# checks vertical
+		if PlayerWon: 
+			return True
+	PlayerWon = Grid[0][0] == Symbol and Grid[1][1] == Symbol and Grid[2][2] == Symbol#diagonals
+	PlayerWon = PlayerWon or Grid[0][2] == Symbol and Grid[1][1] == Symbol and Grid[2][0] == Symbol#diagonals
+	if PlayerWon: 
+		return True
+	return False	
+	
+def PlayerTurnState():  #functions
+	if Players[0] == "Computer":
+		pass
+	else:
+		CurrentPlayer, Symbol = Players[0] #which player is playing
+		X,Y = PromptPlayerMove()
+		if not Grid[X][Y] == 0:
+			print ("That space is already occupied.")
+		else:
+			Grid[X][Y] = Symbol
+			Players.append(Players.pop(0)) #removes first element from list and adds it to the end of the list. maybe use players.reverse.
+			if HasPlayerWon(Symbol):
+				return GameOverState
+	PrintGrid()
+	
+def GameOverState():
+	print ("You are the winner.")
+	input("Press enter to exit.")
+	sys.exit(0)
+	
+CurrentState = OpponentChoiceState #state is executed
+while True:		#endless loop really.
+	ReturnValue = CurrentState()
+	if ReturnValue is not None:
+		CurrentState = ReturnValue #switches to previous state in line 45.
+		
