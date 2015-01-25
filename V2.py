7665b52990cfe5ccff49ebb00603d2648be4d0d8
Exelion <dw14089@my.bristol.ac.uk>
2015-01-25 14:56:19 +0000

    Computer Opponent

--- V2.py
+++ V2.py
@@ -1,19 +1,5 @@
 #Basic 2 Player  Noughts and Crosses Game
-#...................,.....,-~*~,,
-#......................./:.:.:.:.:.|
-#......................|;.;.;.;.;./
-#......................|.;.;.;.;.|
-#............._,,,,,_.).;.;.;.;.|
-#.........,,-":.:.:.:."~-,;.;.;.|
-#........(_,,,,---,,_:.:.);.;.;..",,
-#......,-":.:.:.:.:.""-,,/;.;.;.;.;.",
-#.....(:.__,,,,,,,,,___);.;.;.;.;.;|
-#...../"":.:.:.:.:.:.:""\;.;.;.;.;.,"
-#....\",__,,,,,,,,,,,__/;;;;;;;;;/\
-#.....\.::.:.:.:.:.:.:.;.);;;;;;;;;/:\
-#.......\,,,,,---~~~~;;;;;;;;,"::::\
-#.........."""~~--,,,,,,,,,,-"::::::::::\
-import sys
+import sys, random
 
 Players = [("Player1",1)] #first player in this list will be the player whose turn it is. if a players turn is over then they are removed and appended to the end.
 #players is a list of tuples, first element name second element symbol to represent player choices.
@@ -29,24 +15,33 @@ def OpponentChoiceState():
 	else: 
 		print ("Level 8 OSI Error")
 		return #exits function and goes back to where it was, loop repeats in this case.
-	print ("change current state")
 	return PlayerTurnState #state switch
 	
 
 def PrintGrid():
-	for Row in range(len(Grid)):
-		for Col in range(len(Grid)):
-			print (Grid[Row][Col], end=" ")
-		print(" ")
+    print("--------------")
+    for Row in range(len(Grid)):
+        for Col in range(len(Grid)):
+            Symbol = Grid[Row][Col]
+            if Symbol == 1:
+                Symbol = 'X'
+            elif Symbol == 0:
+                Symbol = '-'
+            else:
+                Symbol = '0'
+            print (Symbol, end=" ")
+        print("")
+    print("--------------")
 
 def ComputerOpponent(Symbol):
-	print("Computer is making a move.")
-	for Row in range(len(Grid)):
-		for Col in range(len(Grid)):
-			Grid[Row][Col] == Symbol
-	PrintGrid()
-		
-		
+    while True:
+        X = random.randint(0, 2)
+        Y = random.randint(0, 2)
+        if Grid[X][Y] == 0:
+            Grid[X][Y] = Symbol
+            break
+    print("The computer has made its move.")
+
 def PromptInt(Message): #Catches user errors when a non integer is entered. try is kind of like a virtual machine environment.
 	try: # tests what the user entered.
 		Number = int(input(Message)) #returns verified integer.
@@ -59,11 +54,11 @@ def PromptInt(Message): #Catches user errors when a non integer is entered. try 
 		return PromptInt(Message) #recursively asks the user for a valid input.
 		
 def PromptPlayerMove():
-	print ("Make your move player.")
-	X = PromptInt("X")-1# -1 is used because the range is 0-2, however most users will interpret the board as being from 1-3.
-	Y = PromptInt("Y")-1
-	return X,Y
-	
+    print ("Make your move player.")
+    X = PromptInt("X: ")-1# -1 is used because the range is 0-2, however most users will interpret the board as being from 1-3.
+    Y = PromptInt("Y: ")-1
+    return X,Y
+    	
 def HasPlayerWon(Symbol):
 	  
 	for Row in range(3): #execute this block of code for each row.Holds true output in PlayerWon
@@ -78,28 +73,35 @@ def HasPlayerWon(Symbol):
 	return False	
 	
 def PlayerTurnState():  #functions
-	CurrentPlayer, Symbol = Players[0] #which player is playing
-	if CurrentPlayer=="Computer":
-		ComputerOpponent(Symbol) #
-	else:
-		X,Y = PromptPlayerMove()
-		if not Grid[X][Y] == 0:
-			print ("That space is already occupied.")
-			return
-		Grid[X][Y] = Symbol
-		Players.append(Players.pop(0)) #removes first element from list and adds it to the end of the list. maybe use players.reverse.
-	if HasPlayerWon(Symbol):
-		return GameOverState
-	PrintGrid()
+    CurrentPlayer, Symbol = Players[0] #which player is playing
+    if CurrentPlayer=="Computer":
+        ComputerOpponent(Symbol)
+    else:
+        X,Y = PromptPlayerMove()
+        if not Grid[X][Y] == 0:
+            print ("That space is already occupied.")
+            return
+        Grid[X][Y] = Symbol
+    PrintGrid()
+    if HasPlayerWon(Symbol):
+        return GameOverState
+    Players.append(Players.pop(0)) #removes first element from list and adds it to the end of the list. maybe use players.reverse.
 	
 def GameOverState():
-	print ("You are the winner.")
-	input("Press enter to exit.")
-	sys.exit(0)
+    if Players[0][0] == 'Computer':
+        print("The computer wins!")
+    else:
+        print("{} wins!".format(Players[0][0]))
+    input("Press enter to exit.")
+    sys.exit(0)
+
+def Main():
+    CurrentState = OpponentChoiceState #state is executed
+    while True:		#endless loop really.
+        ReturnValue = CurrentState()
+        if ReturnValue is not None:
+            CurrentState = ReturnValue #switches to previous state in line 45.
 	
-CurrentState = OpponentChoiceState #state is executed
-while True:		#endless loop really.
-	ReturnValue = CurrentState()
-	if ReturnValue is not None:
-		CurrentState = ReturnValue #switches to previous state in line 45.
-		
+if __name__ == '__main__':
+    Main()
+
